be18e4aee639f3838b3c6ed1a30277489e8ff213
Johny <yonniedooo@gmail.com>
2022-12-29 22:13:21 +1100

    update grid generation functionality to include removal of prev grid

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
-
 const container = document.querySelector("div");
 const grid = document.createElement("div");
 grid.className = 'grid'
 container.appendChild(grid);
 
 gridGenerator(16, 16)
+/*for (let i = 0; i < 16; i++) {
+    const ogColumn = document.createElement('div');
+    ogColumn.className = 'ogColumn';
+    
+    for (let j = 0; j < 16; j++) {
+        const ogRow = document.createElement("div");
+        ogRow.className = 'row'
+        ogRow.textContent = 'LFG'
+        ogColumn.appendChild(ogRow);
 
-//function gridRemover() {}
-
+        ogRow.addEventListener('mouseenter', () => {
+            document.getElementsByClassName("ogRow")
+            for (let k=0; k < 1; k++) {
+            ogRow.style.backgroundColor = 'blue';
+        }});
+    };
+    grid.appendChild(ogColumn);
+};*/
 function gridGenerator(columns, rows) {
-
     for (let i = 0; i < columns; i++) {
         const column = document.createElement('div');
         column.className = 'column';
@@ -25,15 +38,18 @@ function gridGenerator(columns, rows) {
                 for (let k=0; k < 1; k++) {
                 row.style.backgroundColor = 'blue';
             }});
-    
         };
         grid.appendChild(column);
     };
 };
 
+function gridRemover() {
+    grid.innerHTML = ' ';
+}
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
+    gridRemover();
     let ans = prompt("How many squares per side would you like?")
     gridGenerator(ans, ans);
 });
