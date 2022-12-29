6345383528fb62a83f48a1f5e2ceee0d16a608d8
Johny <yonniedooo@gmail.com>
2022-12-30 01:24:57 +1100

    release version 1.0.0

--- javascript.js
+++ javascript.js
@@ -3,25 +3,6 @@ const grid = document.createElement("div");
 grid.className = 'grid'
 container.appendChild(grid);
 
-gridGenerator(16, 16)
-/*for (let i = 0; i < 16; i++) {
-    const ogColumn = document.createElement('div');
-    ogColumn.className = 'ogColumn';
-    
-    for (let j = 0; j < 16; j++) {
-        const ogRow = document.createElement("div");
-        ogRow.className = 'row'
-        ogRow.textContent = 'LFG'
-        ogColumn.appendChild(ogRow);
-
-        ogRow.addEventListener('mouseenter', () => {
-            document.getElementsByClassName("ogRow")
-            for (let k=0; k < 1; k++) {
-            ogRow.style.backgroundColor = 'blue';
-        }});
-    };
-    grid.appendChild(ogColumn);
-};*/
 function gridGenerator(columns, rows) {
     for (let i = 0; i < columns; i++) {
         const column = document.createElement('div');
@@ -30,26 +11,33 @@ function gridGenerator(columns, rows) {
         for (let j = 0; j < rows; j++) {
             const row = document.createElement("div");
             row.className = 'row'
-            row.textContent = 'LFG'
+            row.textContent = ' '
             column.appendChild(row);
 
             row.addEventListener('mouseenter', () => {
                 document.getElementsByClassName("row")
                 for (let k=0; k < 1; k++) {
-                row.style.backgroundColor = 'blue';
+                row.style.backgroundColor = '#b1acac';
             }});
         };
         grid.appendChild(column);
     };
 };
 
+gridGenerator(16, 16)
+
 function gridRemover() {
-    grid.innerHTML = ' ';
-}
+    grid.innerHTML = ' ';}
 
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     gridRemover();
-    let ans = prompt("How many squares per side would you like?")
-    gridGenerator(ans, ans);
+    const ans = prompt("How many squares per side would you like?")
+    if (ans > 100) {
+        alert("grid size limit exceeded")
+        gridGenerator(99, 99);
+    }
+    else {
+        gridGenerator(ans, ans);
+    };
 });
